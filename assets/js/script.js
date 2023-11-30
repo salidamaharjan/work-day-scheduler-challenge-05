559a58ffeafa3c1e2d3498a7289ad6e3869a1c6a
salidamaharjan <salidamaharjan@outlook.com>
2023-11-30 09:04:56 -0600

    creating a variable in JS from  provided HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,16 @@
 $(function () {
   // locating the div with class container-lg
   var $mainContainer = $(".container-lg");
-  
+
+  //creating a variable to store the row which has three column
+  var $row = $(`<div id="hour-9" class="row time-block past">
+  <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+  <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+    <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
+</div> 
+`);
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
