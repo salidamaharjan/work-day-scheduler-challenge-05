e2dc1676eb679c0974c2f2483dced11fe00b24d6
salidamaharjan <salidamaharjan@outlook.com>
2023-12-01 13:01:49 -0600

    adding console.log to see the behaviour of the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,26 @@
 $(function () {
   // locating the div with class container-lg
   var $mainContainer = $(".container-lg");
+  console.log("mainContainer");
 
   //displaying current date on the header
   var $pTag = $("#currentDay");
+  console.log("$pTag");
   $pTag.append(dayjs().format("dddd, MMMM Do"));
+  console.log("$pTag.append(dayjs()");
 
   // I want to create 9 rows so using for loop
-  for (var i = 9; i < 18; i++) {
+  for (var i = 0; i < 18; i++) {
+    console.log("entering for loop");
     creatingRow(i);
+    console.log("exiting for loop");
   }
 
   function creatingRow(hour) {
+    console.log("starting of creatingRow function");
     //creating a variable to store the row which has three column
     //using string interpolation to add the hour passed when called
+    console.log("before displayTimeAmPm");
     var displayTimeAmPm = displayTime(hour) + isAmOrPM(hour);
     var $row =
       $(`<div id="hour-${hour}" class="row time-block ${isPastPresentFuture(
@@ -29,29 +36,38 @@ $(function () {
   </button>
 </div> 
 `);
-
     // finding a button created in a row
+    var $text = $row.find("textarea");
     var $saveBtn = $row.find(".saveBtn");
-    clickedSaveBtn($saveBtn);
+
+    clickedSaveBtn($saveBtn, $text, hour);
+    console.log("click save button called");
 
     //adding the a row to the main div
     $mainContainer.append($row);
+    console.log("exiting creating row function");
   }
 
   //want to add AM and PM to the displayed time
   function isAmOrPM(hour) {
+    console.log("starting isAmOrPm function");
     if (hour < 12) {
+      console.log("exiting isampm from if");
       return "AM";
     } else {
+      console.log("exiting isampm from else");
       return "PM";
     }
   }
   //I want to change military time to normal time
   function displayTime(hour) {
+    console.log("entering displayTime function");
     var time = hour;
     if (hour <= 12) {
+      console.log("exiting displayTime function from if");
       return time;
     } else {
+      console.log("exiting displayTime function from else");
       time = time - 12;
       return time;
     }
@@ -60,19 +76,28 @@ $(function () {
 
   //need to change the color according to the current time
   function isPastPresentFuture(comparedHour) {
+    console.log("entering ispastpresentfuture function");
     var dayFormat = parseInt(dayjs().format("HH"));
     console.log(typeof dayFormat);
     if (comparedHour < dayFormat) {
+      console.log("exiting ispastpresentfuture function from if");
       return "past";
     } else if (comparedHour > dayFormat) {
+      console.log("exiting ispastpresentfuture function from else if");
       return "future";
     } else {
+      console.log("exiting ispastpresentfuture function from else");
       return "present";
     }
   }
-  function clickedSaveBtn($btn) {
+  //adding event listener on click and storing to local storage
+  function clickedSaveBtn($btn, $text, hour) {
+    var hourTask = hour + "-" + "task";
     $btn.on("click", function () {
-      alert("You clicked me");
+      console.log("entering eventlistener function");
+      console.log($text.val());
+      localStorage.setItem(hourTask, $text.val());
+      console.log("exiting clickSaveButton function");
     });
   }
 
