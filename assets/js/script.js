952418503f17efcace6b95cc38b772dcddc2f9e3
salidamaharjan <salidamaharjan@outlook.com>
2023-12-01 19:58:17 -0600

    rearranging code and deleting console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,19 @@
 $(function () {
   // locating the div with class container-lg
   var $mainContainer = $(".container-lg");
-  console.log("mainContainer");
 
   //displaying current date on the header
   var $pTag = $("#currentDay");
-  console.log("$pTag");
   $pTag.append(dayjs().format("dddd, MMMM Do"));
-  console.log("$pTag.append(dayjs()");
 
   // I want to create 9 rows so using for loop
   for (var i = 9; i < 18; i++) {
-    console.log("entering for loop");
     creatingRow(i);
-    console.log("exiting for loop");
   }
 
   function creatingRow(hour) {
-    console.log("starting of creatingRow function");
     //creating a variable to store the row which has three column
     //using string interpolation to add the hour passed when called
-    console.log("before displayTimeAmPm");
     var displayTimeAmPm = displayTime(hour) + isAmOrPM(hour);
     var $row =
       $(`<div id="hour-${hour}" class="row time-block ${isPastPresentFuture(
@@ -40,19 +33,16 @@ $(function () {
     var $text = $row.find("textarea");
     var $saveBtn = $row.find(".saveBtn");
 
-    //when the page is refreshed onload function is called
-    if(location.reload){
-      onload(hour);
-      console.log("line 46 ->",onload(hour));
-      //displaying the stored value on the text area
-      $text.append(onload(hour));
-    }
+    //retrieving data from local storage
+    getFromLocalStorage(hour);
+
+    //displaying the stored value on the text area
+    $text.append(getFromLocalStorage(hour));
+
     clickedSaveBtn($saveBtn, $text, hour);
-    console.log("click save button called");
 
     //adding the a row to the main div
     $mainContainer.append($row);
-    console.log("exiting creating row function");
   }
 
   //want to add AM and PM to the displayed time
@@ -68,32 +58,23 @@ $(function () {
   }
   //I want to change military time to normal time
   function displayTime(hour) {
-    console.log("entering displayTime function");
     var time = hour;
     if (hour <= 12) {
-      console.log("exiting displayTime function from if");
       return time;
     } else {
-      console.log("exiting displayTime function from else");
       time = time - 12;
       return time;
     }
   }
-  console.log("to check the function display time is working or not->",displayTime(14));
 
   //need to change the color according to the current time
   function isPastPresentFuture(comparedHour) {
-    console.log("entering ispastpresentfuture function");
     var dayFormat = parseInt(dayjs().format("HH"));
-    console.log(typeof dayFormat);
     if (comparedHour < dayFormat) {
-      console.log("exiting ispastpresentfuture function from if");
       return "past";
     } else if (comparedHour > dayFormat) {
-      console.log("exiting ispastpresentfuture function from else if");
       return "future";
     } else {
-      console.log("exiting ispastpresentfuture function from else");
       return "present";
     }
   }
@@ -101,16 +82,12 @@ $(function () {
   function clickedSaveBtn($btn, $text, hour) {
     var hourTask = hour + "-" + "task";
     $btn.on("click", function () {
-      console.log("entering eventlistener function");
-      console.log($text.val());
       localStorage.setItem(hourTask, $text.val());
-      console.log("exiting clickSaveButton function");
     });
   }
-  //a function when the page is reloaded
-  function onload(hour){
-    var hourTask = localStorage.getItem(hour+"-"+"task");
-    console.log("inside the onload function ->", hourTask);
+  //a function to store in local storage
+  function getFromLocalStorage(hour) {
+    var hourTask = localStorage.getItem(hour + "-" + "task");
     return hourTask;
   }
   // TODO: Add a listener for click events on the save button. This code should
